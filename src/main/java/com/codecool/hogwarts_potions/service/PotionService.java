bb334ed51a58a0aa050d7fe11e7ab6d097bf87bb
Philippovitsch <philipp@stadter.at>
2023-02-28 14:51:54 +0100

    Implement logic of PotionService class

--- src/main/java/com/codecool/hogwarts_potions/service/PotionService.java
+++ src/main/java/com/codecool/hogwarts_potions/service/PotionService.java
@@ -1,15 +1,55 @@
 package com.codecool.hogwarts_potions.service;
 
-import com.codecool.hogwarts_potions.model.Potion;
+import com.codecool.hogwarts_potions.model.*;
+import com.codecool.hogwarts_potions.service.repositories.PotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class PotionService {
 
-    public List<Potion> getAllPotions() {
-        //TODO
-        return null;
+    private final PotionRepository potionRepository;
+    private final RecipeService recipeService;
+
+    @Autowired
+    public PotionService(PotionRepository potionRepository, RecipeService recipeService) {
+        this.potionRepository = potionRepository;
+        this.recipeService = recipeService;
     }
+
+    public List<Potion> getAllPotions() {
+        return potionRepository.findAll();
+    }
+
+    public void addPotion(Potion potion) {
+        BrewingStatus brewingStatus = getBrewingStatus(potion);
+        potion.setBrewingStatus(brewingStatus);
+        potionRepository.save(potion);
+    }
+
+    public BrewingStatus getBrewingStatus(Potion potion) {
+        if (potion.getIngredients().size() < 5) {
+            return BrewingStatus.BREW;
+        } else if (isKnownRecipe(potion.getIngredients())) {
+            return BrewingStatus.REPLICA;
+        } else {
+            return BrewingStatus.DISCOVERY;
+        }
+    }
+
+    private boolean isKnownRecipe(Set<Ingredient> ingredients) {
+        for (Recipe recipe : recipeService.getAllRecipes()) {
+            Set<String> potionIngredients = recipe.getIngredients().stream().map(Ingredient::getName).collect(Collectors.toSet());
+            Set<String> recipeIngredients = ingredients.stream().map(Ingredient::getName).collect(Collectors.toSet());
+            if(potionIngredients.equals(recipeIngredients)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
