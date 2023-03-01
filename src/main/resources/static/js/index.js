a458540a11d26b15c6ed1cdcdaaddc650282b64b
Philippovitsch <philipp@stadter.at>
2023-03-01 19:09:22 +0100

    Add basic layout and form to html

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,21 @@
-console.log("ok");
+async function init() {
+    const ingredients = await fetchData("http://localhost:8080/ingredients");
+    addIngredients(ingredients);
+}
+
+async function fetchData(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+function addIngredients(ingredients) {
+    const ingredientInput = document.querySelector("#ingredients");
+    ingredients.forEach(ingredient => {
+        const option = document.createElement("option");
+        option.value = ingredient.name;
+        option.innerText = ingredient.name;
+        ingredientInput.append(option);
+    });
+}
+
+init();
