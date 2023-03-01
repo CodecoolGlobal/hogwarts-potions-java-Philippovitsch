13a75d1e7a3cd628448239e034702e2dfd5a21b5
Philippovitsch <philipp@stadter.at>
2023-03-01 12:36:09 +0100

    Simplify PotionDTO

--- src/main/java/com/codecool/hogwarts_potions/model/dto/PotionDTO.java
+++ src/main/java/com/codecool/hogwarts_potions/model/dto/PotionDTO.java
@@ -1,6 +1,5 @@
 package com.codecool.hogwarts_potions.model.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,17 +7,10 @@ import java.util.List;
 
 @Getter
 @Setter
-@Builder
 public class PotionDTO {
 
-    private final String potionName;
-    private final Long studentId;
-    private final List<String> ingredients;
-
-    public PotionDTO(String potionName, Long studentId, List<String> ingredients) {
-        this.potionName = potionName;
-        this.studentId = studentId;
-        this.ingredients = ingredients;
-    }
+    private String potionName;
+    private Long studentId;
+    private List<String> ingredients;
 
 }
