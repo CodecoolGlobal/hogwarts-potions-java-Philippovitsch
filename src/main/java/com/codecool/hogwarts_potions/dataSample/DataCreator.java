0aec52e4dcbeb6574d820761b69d026d353425ea
Philippovitsch <philipp@stadter.at>
2023-03-02 14:06:49 +0100

    Improve test data

--- src/main/java/com/codecool/hogwarts_potions/dataSample/DataCreator.java
+++ src/main/java/com/codecool/hogwarts_potions/dataSample/DataCreator.java
@@ -27,11 +27,11 @@ public class DataCreator {
     }
 
     private void initRoomsAndPersons() {
-        Room room1 = Room.builder().name("TestRoom1").capacity(2).residents(new HashSet<>()).build();
-        Room room2 = Room.builder().name("TestRoom2").capacity(2).residents(new HashSet<>()).build();
-        Room room3 = Room.builder().name("TestRoom3").capacity(2).residents(new HashSet<>()).build();
-        Student student1 = Student.builder().name("TestStudent1").houseType(HouseType.GRYFFINDOR).petType(PetType.OWL).build();
-        Student student2 = Student.builder().name("TestStudent2").houseType(HouseType.HUFFLEPUFF).petType(PetType.NONE).build();
+        Room room1 = Room.builder().name("Gryffindor Shack").capacity(2).residents(new HashSet<>()).build();
+        Room room2 = Room.builder().name("Hufflepuff Hut").capacity(2).residents(new HashSet<>()).build();
+        Room room3 = Room.builder().name("Dumbledore Lodge").capacity(2).residents(new HashSet<>()).build();
+        Student student1 = Student.builder().name("Harry Potter").houseType(HouseType.GRYFFINDOR).petType(PetType.OWL).build();
+        Student student2 = Student.builder().name("Hermine Granger").houseType(HouseType.HUFFLEPUFF).petType(PetType.NONE).build();
 
         room1.setResidents(new HashSet<>(Collections.singletonList(student1)));
         room2.setResidents(new HashSet<>(Collections.singletonList(student2)));
@@ -51,13 +51,25 @@ public class DataCreator {
         ingredientService.createNewIngredient("HorseHair");
         ingredientService.createNewIngredient("SnakeWeed");
         ingredientService.createNewIngredient("UnicornHair");
+        ingredientService.createNewIngredient("Frog");
+        ingredientService.createNewIngredient("Liver");
+        ingredientService.createNewIngredient("FireSeed");
 
         List<String> ingredientsList1 = Arrays.asList("Avocado", "Bone", "DragonLiver", "GingerRoot", "HorseHair");
         List<String> ingredientsList2 = Arrays.asList("Bone", "DragonLiver", "GingerRoot", "HorseHair", "SnakeWeed");
-        PotionDTO potionDTO1 = PotionDTO.builder().potionName("TestRecipe1").studentId(1L).ingredients(ingredientsList1).build();
-        PotionDTO potionDTO2 = PotionDTO.builder().potionName("TestRecipe2").studentId(2L).ingredients(ingredientsList2).build();
+        List<String> ingredientsList3 = Arrays.asList("DragonLiver", "GingerRoot", "HorseHair", "SnakeWeed", "UnicornHair");
+        List<String> ingredientsList4 = Arrays.asList("GingerRoot", "HorseHair", "SnakeWeed", "UnicornHair", "Frog");
+        List<String> ingredientsList5 = Arrays.asList("HorseHair", "SnakeWeed", "UnicornHair", "Frog", "Liver");
+        PotionDTO potionDTO1 = PotionDTO.builder().potionName("Baneberry Potion").studentId(1L).ingredients(ingredientsList1).build();
+        PotionDTO potionDTO2 = PotionDTO.builder().potionName("Draught of Peace").studentId(1L).ingredients(ingredientsList2).build();
+        PotionDTO potionDTO3 = PotionDTO.builder().potionName("Invisibility Potion").studentId(1L).ingredients(ingredientsList3).build();
+        PotionDTO potionDTO4 = PotionDTO.builder().potionName("Sleeping Draught").studentId(2L).ingredients(ingredientsList4).build();
+        PotionDTO potionDTO5 = PotionDTO.builder().potionName("Weedosoros").studentId(2L).ingredients(ingredientsList5).build();
         potionService.addPotion(potionDTO1);
         potionService.addPotion(potionDTO2);
+        potionService.addPotion(potionDTO3);
+        potionService.addPotion(potionDTO4);
+        potionService.addPotion(potionDTO5);
 
         Long potionId1 = potionService.createNewPotion(1L).getId();
         potionService.addIngredient(potionId1, IngredientDTO.builder().ingredientName("Avocado").build());
